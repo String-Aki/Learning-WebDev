029e80a719494754718fe921cf02609c9ec6c442
Aki-1407 <im.deepak1407@gmail.com>
2025-06-03 20:11:40 +0530

    Replicating the Guided project by myself.

---
+++ Learn React/Projects/ReactFacts Final/ReactFacts V-Final/src/App.jsx
@@ -0,0 +1,10 @@
+import NavBar from "./components/NavBar"
+import MainContent from "./components/MainContent"
+export default function App(){
+    return (
+        <>
+            <NavBar />
+            <MainContent />
+        </>
+    )
+}
---
+++ Learn React/Projects/ReactFacts Final/ReactFacts V-Final/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return (
+        <header>
+            <nav>
+                <img src="./src/assets/react-logo.png" alt="react-logo" className="logo"/>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
